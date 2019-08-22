699f84ed27c15a9f4edd076b2c6f9ad6010ace5e
Hatem <zehani.hatem@gmail.com>
2019-08-23 01:59:12 +0200

    Updates

---
+++ precache-manifest.815a5721d845a1e6d60b2363aa6d0e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7b86fc9c47529c7e54b86b519ab427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ea5f0469aef16a2daa4",
+    "url": "/static/css/main.0d1b6c11.chunk.css"
+  },
+  {
+    "revision": "9bc9740e9201ff6c1bfa",
+    "url": "/static/js/2.702315ab.chunk.js"
+  },
+  {
+    "revision": "6ea5f0469aef16a2daa4",
+    "url": "/static/js/main.4c363faa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
