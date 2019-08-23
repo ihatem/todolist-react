aa598526f4321c9bdc2c06d54b787b9477b213b3
Hatem <zehani.hatem@gmail.com>
2019-08-23 02:42:46 +0200

    Updates

---
+++ precache-manifest.1655bc95c558fad3c21bafa3c8db4bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2e6172ba3242504c91d61e703247d1",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "2c5ab5f77e88146ad225",
+    "url": "/todolist-react/static/css/main.0d1b6c11.chunk.css"
+  },
+  {
+    "revision": "9bc9740e9201ff6c1bfa",
+    "url": "/todolist-react/static/js/2.702315ab.chunk.js"
+  },
+  {
+    "revision": "2c5ab5f77e88146ad225",
+    "url": "/todolist-react/static/js/main.14f27176.chunk.js"
+  },
+  {
+    "revision": "c6a1de0b206601e973bd",
+    "url": "/todolist-react/static/js/runtime~main.1492b700.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/todolist-react/precache-manifest.542ff05af9f720507efb0797c9d52821.js"
+  "/todolist-react/precache-manifest.1655bc95c558fad3c21bafa3c8db4bea.js"
 );
 
 self.addEventListener('message', (event) => {
